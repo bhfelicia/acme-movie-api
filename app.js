a41f2013bfb51ecabc8a477d90601781169ee41a
Felicia Heiney <bhfelicia@gmail.com>
2021-04-22 21:17:06 -0400

    runs mocha with --exit flag

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const {
+  models: { Movie, Actor, Role },
+} = require("./db");
 const path = require("path");
 
 const app = express();
@@ -8,6 +11,24 @@ app.use(express.json());
 app.get("/", (req, res, next) =>
   res.sendFile(path.join(__dirname, "index.html"))
 );
+
+app.get("/api/movies", async (req, res, next) => {
+  try {
+    const movies = await Movie.findAll();
+    res.send(movies);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/actors", async (req, res, next) => {
+  try {
+    const actors = await Actor.findAll();
+    res.send(actors);
+  } catch (error) {
+    next(error);
+  }
+});
 
 const port = process.env.PORT || 3000;
 
--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,5 @@
 const app = require("supertest")(require("../app"));
-const {
-  syncAndSeed,
-  models: { Movie, Actor, Role },
-} = require("../db");
+// const { syncAndSeed } = require("../db");
 
 const { expect } = require("chai");
 
@@ -13,12 +10,33 @@ describe("addition", () => {
 });
 
 describe("Routes", () => {
-  beforeEach(async () => syncAndSeed());
+  // beforeEach(async () => await syncAndSeed());
   describe("GET /", () => {
     it("returns an html page", async () => {
       try {
         const response = await app.get("/");
         expect(response.status).to.equal(200);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
+  describe("GET /api/movies", () => {
+    it("returns movies", async () => {
+      try {
+        const movies = await app.get("/api/movies");
+        expect(movies.status).to.equal(200);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
+  describe("GET /api/actors", () => {
+    it("retrieves our actors from the db", async () => {
+      try {
+        const response = await app.get("/api/actors");
+        expect(response.status).to.equal(200);
+        console.log("hi");
       } catch (error) {
         console.log(error);
       }
